72a6cca46a17f728e43fcc808bcfe0f073a2b21e
MayLynCom <mayconsouza0712@outlook.com>
2022-09-13 11:24:59 -0300

    nao se pode mais ter elementos duplicados

--- ListaLigada/ListaLigadaOrdenada/ListaLigadaOrdenada.cpp
+++ ListaLigada/ListaLigadaOrdenada/ListaLigadaOrdenada.cpp
@@ -125,38 +125,44 @@ void inserirElemento()
 	cin >> novo->valor;
 	novo->prox = NULL;
 
-	if (primeiro == NULL)
-	{
-		primeiro = novo;
+	NO* encontrado = posicaoElemento(novo->valor);
+	if (encontrado == NULL) {
+		if (primeiro == NULL)
+		{
+			primeiro = novo;
+		}
+		else
+		{
+			NO* aux = primeiro;
+
+			NO* deslocar = NULL;
+			while (aux->prox != NULL) {
+				if (novo->valor < aux->prox->valor) {
+					deslocar = aux->prox;
+					aux->prox = novo;
+					novo->prox = deslocar;
+					cout << "adicionado" << endl;
+					break;
+				}
+				else {
+					aux = aux->prox;
+				}
+			}
+			while (aux->prox == NULL) {
+				if (novo->valor < aux->valor) {
+					primeiro = novo;
+					novo->prox = aux;
+				}
+				else {
+					cout << "adicionado" << endl;
+
+					aux->prox = novo;
+				}
+			}
+		}
 	}
-	else
-	{
-		NO* aux = primeiro;
-
-		NO* deslocar = NULL;
-		while (aux->prox != NULL) {
-			if (novo->valor <= aux->prox->valor) {
-				deslocar = aux->prox;
-				aux->prox = novo;
-				novo->prox = deslocar;
-				cout << "adicionado" << endl;
-				break;
-			}
-			else {
-				aux = aux->prox;
-			}
-		}
-		while (aux->prox == NULL) {
-			if (novo->valor < aux->valor) {
-				primeiro = novo;
-				novo->prox = aux;
-			}
-			else {
-				cout << "adicionado" << endl;
-
-				aux->prox = novo;
-			}
-		}
+	else {
+		cout << "Elemento ja existe na lista\n";
 	}
 }
 
@@ -167,7 +173,28 @@ void excluirElemento()
 
 void buscarElemento()
 {
-
+	int valor;
+	cout << "Digite o elemento que deseja buscar: \n";
+	cin >> valor;
+	NO* encontrado = posicaoElemento(valor);
+	if (encontrado != NULL) {
+		
+	}
+	else {
+		cout << "Elemento nao encontrado!\n";
+	}
 }
 
 
+NO* posicaoElemento(int numero)
+{
+	NO* aux = primeiro;
+	while (aux != NULL) {
+		if (aux->valor == numero)
+		{
+			break;
+		}
+		aux = aux->prox;
+	}
+	return aux;
+}
